04fd54aa6045213281a4617f7da7d5dc836e6496
andre <andre.giovannetti.ext@nokia.com>
2021-07-14 14:46:49 -0300

    Finished version

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Exercise from Python PRO course module 4.
 
 Scraping https://www.w3schools.io/file/yaml-sample-example/ for a YAML file, getting the comments from the page with REGEX then making my own YAML file.
-s
+
+
+During development I tried using a dict to create the YAML file, howerver I was confuse since dict require Keys:Value and some of the data from the example YAML file were only normal sentences.So I decided to use a normal list.
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 from requests import get
 import re   
 import html5lib
+from yaml   import dump
+
+#--------------- scraping the site----------------------------------
 
 response = get('https://www.w3schools.io/file/yaml-sample-example/')
 soup = BeautifulSoup(response.text, "html5lib")
@@ -9,3 +12,32 @@ title = soup.find('title').text
 print(f'Titulo da página: {title}\n')
 content = soup.find("pre")
 exemplo_yml= content.text
+
+#--------------- creating .txt file with scraped data.
+
+exemplo_txt= open("exemplo.txt", "w")
+exemplo_txt.write(exemplo_yml)
+exemplo_txt.close()
+
+#--------------- iterating the txt file to filter out the comments, \n and ''(null strings).
+
+lista_linhas_yml=[]
+exemplo_txt = open("exemplo.txt", 'r')
+for linha in exemplo_txt:
+    if '#'in linha:
+        line = re.sub(r'#.*$', '', linha)
+        lista_linhas_yml.append(line)
+    else:
+        lista_linhas_yml.append(linha)
+exemplo_txt.close()
+
+nova_yml=[]
+for i in lista_linhas_yml:
+    nova_yml.append(i.strip())
+
+while '' in nova_yml:
+    nova_yml.remove('')
+
+#------------------ creating .yml file
+with open ("new_yml.yml","w") as file:
+    dump(nova_yml,file)
